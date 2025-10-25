c8133aa05fa487dfe15cd62114c5dd2e5b89a929
khalid0407 <khalid.a.adan@hotmail.com>
2025-10-25 03:41:44 +0200

    update

--- Chatify-app/vite.config.js
+++ Chatify-app/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
   plugins: [react()],
+  build: {
+    copyPublicDir: true, 
+  },
   server: {
     proxy: {
       "/csrf": {
